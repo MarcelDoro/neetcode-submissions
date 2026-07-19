5b0746d04b35ed8d41a4c4124163ef689cab491a
MarcelDoro <marcel.do@onet.pl>
2026-07-19 15:08:44 +0200

    Add: find-median-in-a-data-stream - submission-5

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-5.py
@@ -0,0 +1,29 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small_heap = [] #max_heap
+        self.large_heap = [] #min_heap
+        heapq.heapify_max(self.small_heap)
+        heapq.heapify(self.large_heap)
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush_max(self.small_heap, num)        
+        if self.small_heap and self.large_heap and self.small_heap[0] > self.large_heap[0]:
+            val = heapq.heappop_max(self.small_heap)
+            heapq.heappush(self.large_heap, val)
+        
+        if len(self.small_heap) - len(self.large_heap) > 1:
+            val = heapq.heappop_max(self.small_heap)
+            heapq.heappush(self.large_heap, val)
+        if len(self.large_heap) - len(self.small_heap) > 1:
+            val = heapq.heappop(self.large_heap)
+            heapq.heappush_max(self.small_heap, val)
+
+    def findMedian(self) -> float:
+        if len(self.small_heap) > len(self.large_heap):
+            return self.small_heap[0]
+        elif len(self.small_heap) < len(self.large_heap):
+            return self.large_heap[0]
+        else:
+            a, b = self.small_heap[0], self.large_heap[0]
+            return (a + b) / 2
