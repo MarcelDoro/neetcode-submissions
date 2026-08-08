8a39404dfcdab6e7c8d183758ccb0a40555b8bfe
MarcelDoro <marcel.do@onet.pl>
2026-08-08 15:40:09 +0200

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        do_not_change = set()
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+        def add_to_set(r: int, c: int, do_not_change: set()) -> None:
+            if (r < 0 or r >= ROWS or
+                c < 0 or c >= COLS or
+                board[r][c] == 'X' or
+                (r, c) in do_not_change):
+
+                return 
+            
+            do_not_change.add((r, c))
+
+            for dr, dc in directions:
+                new_r, new_c = r + dr, c + dc
+                add_to_set(new_r, new_c, do_not_change)
+
+
+        for r in range(ROWS):
+            add_to_set(r, 0, do_not_change)
+            add_to_set(r, COLS - 1, do_not_change)
+
+        for c in range(COLS):
+            add_to_set(0, c, do_not_change)
+            add_to_set(ROWS - 1, c, do_not_change)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O' and (r, c) not in do_not_change:
+                    board[r][c] = 'X'
