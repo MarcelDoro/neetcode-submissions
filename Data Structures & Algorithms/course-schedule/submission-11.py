63b8fd87f808a7e642bad1f08bcdedf1de534bbc
MarcelDoro <marcel.do@onet.pl>
2026-08-13 15:16:54 +0300

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [ [] for _ in range(numCourses) ]
+
+        for v, u in prerequisites:
+            adj[v].append(u)
+
+        states = [0] * numCourses
+        # 0 - unvisited, 1 - curently visited, 2 - reviously visited
+                
+        def dfs(node: int) -> bool:            
+            if states[node] == 1:
+                return False
+            if states[node] == 2:
+                return True
+
+            states[node] = 1
+            for neighbour in adj[node]:    
+                if not dfs(neighbour):
+                    return False
+            
+            states[node] = 2
+            return True
+
+
+        for n in range(numCourses):     
+            if dfs(n) == False:
+                return False
+
+        return True
+                
